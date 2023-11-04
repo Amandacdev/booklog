2e4fe004fa97f4b49062452cf99186a8151fa15a
IanRibeiroDev <ian.ribeiro@academico.ifpb.edu.br>
2023-11-04 18:16:37 -0300

    Realizando alguns ajustes...de novo >:(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 
 import {MatBadgeModule} from '@angular/material/badge';
-import {AtividadeModule} from "./atividade/atividade.module";
+import {BookModule} from "./book/book.module";
 import {MatChipsModule} from '@angular/material/chips';
 import{AppRoutingModule} from "./app-routing.module";
 import {MenuComponent} from "./layout/menu/menu.component";
@@ -34,7 +34,7 @@ import {MatMenuModule} from "@angular/material/menu";
     MatIconModule,
     MatCardModule,
     MatBadgeModule,
-    AtividadeModule,
+    BookModule,
     AppRoutingModule,
     MatChipsModule,
     MatMenuModule
