df0e664702e36a2496885bf8c45440e68a66a816
Amandacdev <cruz.amanda@academico.ifpb.edu.br>
2023-11-05 03:53:18 -0300

    Ajustes/desenvolv. dos cards e do front

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -13,6 +13,7 @@ import {MatBadgeModule} from '@angular/material/badge';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatChipsModule} from "@angular/material/chips";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import {MatChipsModule} from "@angular/material/chips";
         MatBadgeModule,
         MatIconModule,
         MatDividerModule,
-        MatChipsModule
+        MatChipsModule,
+        MatTooltipModule
     ]
 })
 export class BookModule { }
--- src/app/shared/model/BOOKS.ts
+++ src/app/shared/model/BOOKS.ts
@@ -1,9 +1,26 @@
 import {Book} from "./book";
 
 export const BOOKS = [
-    new Book('Harry Potter','J. K. Rowling','Era uma vez um menino que sobreviveu....',
-        'https://m.media-amazon.com/images/I/710ESoXqVPL._SL1200_.jpg', 40.50)
+    new Book('Harry Potter','J. K. Rowling','Harry Potter é um garoto órfão que vive infeliz com seus tios, os Dursleys. Ele recebe uma carta contendo um convite para ingressar em Hogwarts, uma famosa escola especializada em formar jovens bruxos. Inicialmente, Harry é impedido de ler a carta por seu tio, mas logo recebe a visita de Hagrid, o guarda-caça de Hogwarts, que chega para levá-lo até a escola. Harry adentra um mundo mágico que jamais imaginara, vivendo diversas aventuras com seus novos amigos, Rony Weasley e Hermione Granger.',
+        'https://m.media-amazon.com/images/I/710ESoXqVPL._SL1200_.jpg', 40.50),
+    new Book('A Revolução dos Bichos','George Orwell','Verdadeiro clássico moderno, concebido por um dos mais influentes escritores do século XX, A revolução dos bichos é uma fábula sobre o poder. Narra a insurreição dos animais de uma granja contra seus donos. Progressivamente, porém, a revolução degenera numa tirania ainda mais opressiva que a dos humanos.',
+        'https://m.media-amazon.com/images/I/91BsZhxCRjL._SL1500_.jpg', 25.20),
+    new Book('Tudo é Rio','Carla Madeira','Com uma narrativa madura, precisa e ao mesmo tempo delicada e poética, o romance narra a história do casal Dalva e Venâncio, que tem a vida transformada após uma perda trágica, resultado do ciúme doentio do marido.',
+        'https://m.media-amazon.com/images/I/816Udvs9O7L._SL1500_.jpg', 48.80),
+    new Book('Capitães da Areia','Jorge Amado','Capitães da areia é uma obra precursora, portanto, modelar. É a partir dela que outras narrativas brasileiras que focam em grupos de adolescentes vivendo na marginalidade arriscada são produzidas, como “Cidade de Deus”, de Paulo Lins, e “Querô”, de Plínio Marcos. As crianças  ainda estão na idade dos sonhos, mas já perspectivam a batalha que é vida adulta; por isso conjecturam uns com os outros, ou intimamente, o que cada um “gostaria de ser”.',
+        'https://m.media-amazon.com/images/I/816CKGW3kXL._SL1500_.jpg', 45.10),
+
+
+    new Book('Maus','Art Spiegelman','Clássico contemporâneo dos quadrinhos, Maus é um relato comovente sobre Auschwitz e um acerto de contas do artista com o pai. Única história em quadrinhos a receber o Prêmio Pulitzer.',
+        'https://m.media-amazon.com/images/I/916IgqQ-54L._SL1500_.jpg', 68.80),
+    new Book('Sapiens','Yuval Noah Harari','Sapiens é a obra-prima de Yuval Noah Harari e o consagrou como um dos pensadores mais brilhantes da atualidade. Num feito surpreendente, que já fez deste livro um clássico contemporâneo, o historiador israelense aplica uma fascinante narrativa histórica a todas as instâncias do percurso humano sobre a Terra. Da Idade da Pedra ao Vale do Silício, temos aqui uma visão ampla e crítica da jornada em que deixamos de ser meros símios para nos tornarmos os governantes do mundo.',
+        'https://m.media-amazon.com/images/I/71-ghLb8qML._SL1500_.jpg', 75.10),
+    new Book('O Nome do Vento','Patrick Rothfuss','Ninguém sabe ao certo quem é o herói ou o vilão desse fascinante universo criado por Patrick Rothfuss. Na realidade, essas duas figuras se concentram em Kote, um homem enigmático que se esconde sob a identidade de proprietário da hospedaria Marco do Percurso.',
+        'https://m.media-amazon.com/images/I/81CGmkRG9GL._SL1500_.jpg', 55.10),
+    new Book('A Teia de Charlotte','E B. White','Em uma manhã de primavera, a garotinha Fern resgata um porquinho e o nomeia Wilbur. Vendido para o tio de Fern e mandado para a fazenda dele, o porquinho faz amizade com os animais de lá ― incluindo a gentil aranha Charlotte. Mas, quando Wilbur precisa enfrentar o cruel destino de sua espécie, Fern e Charlotte se juntam aos outros animais para bolarem um plano infalível e salvarem esse porquinho muito especial.',
+        'https://m.media-amazon.com/images/I/91RdYbmF5PL._SL1500_.jpg', 33.10)
 ];
+
 
 
 
